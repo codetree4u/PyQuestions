910b1e34b23cf392d4a68618a5e187f9f816e0db
Carlos Raymundo <80983254+codetree4u@users.noreply.github.com>
2023-01-17 11:24:31 -0800

    Update FindTheMajorityElementFromList.py

--- FindTheMajorityElementFromList.py
+++ FindTheMajorityElementFromList.py
@@ -1,13 +1,9 @@
 """
 Find the majority element from the given list of size n.
-
 The majority element in a list is the element that appears more than n/2 times.
-
 For example,
-
 numbers = [1, 7, 8, 7, 7, 7]
 Here, 7 is the majority element in the given list. It's because 7 has occurred 4 times, which is greater than 6 / 2, which is 3.
-
 Assumption: We will assume that the list is non-empty and the majority element is always present in the list.
 """
 # Write code here  
