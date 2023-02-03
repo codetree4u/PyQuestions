d242131da9ee03d522a3035c11f6f9a04d9b3c35
Carlos Raymundo <80983254+codetree4u@users.noreply.github.com>
2023-02-02 16:36:42 -0800

    Update sorted_anagrams_str.py

--- sorted_anagrams_str.py
+++ sorted_anagrams_str.py
@@ -49,3 +49,25 @@ Expected Output
 
 The two strings are anagrams
 """
+
+# define a function that checks
+# if the strings are anagrams or not
+def is_anagram(str1, str2):
+
+    # remove spaces from the strings
+    str1.replace(" ","")
+    str2.replace(" ","")
+
+    # sort the strings
+    str1 = sorted(str1)
+    str2 = sorted(str2)
+
+    # check if the strings are anagrams or not
+    if str1 == str2:
+        print('The two strings are anagrams')
+    else:
+        print('The two strings are not anagrams')
+
+
+# call the function
+is_anagram('listen', 'silent')
