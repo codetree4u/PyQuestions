392d03603c572234aa621447ba649ea9a9d387cf
Carlos Raymundo <80983254+codetree4u@users.noreply.github.com>
2023-02-21 17:35:55 -0800

    Create char_dig_symb_cnt.py

---
+++ char_dig_symb_cnt.py
@@ -0,0 +1,18 @@
+"""
+Question
+From a given string, count the number of alphabets, digits, and symbols.
+
+For example,
+
+If the given string is,
+
+Hello123.
+the program should return,
+
+Alphabets: 5
+Digits: 3
+Symbols: 1
+Assumption: We will assume that spaces are counted as symbols.
+"""
+
+
