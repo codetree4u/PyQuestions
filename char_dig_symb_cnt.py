38031e4c962939e0d9911a6cd1bda04ac4cb8272
Carlos Raymundo <80983254+codetree4u@users.noreply.github.com>
2023-02-21 17:36:31 -0800

    Update char_dig_symb_cnt.py

--- char_dig_symb_cnt.py
+++ char_dig_symb_cnt.py
@@ -14,5 +14,37 @@ Digits: 3
 Symbols: 1
 Assumption: We will assume that spaces are counted as symbols.
 """
+"""
+Thought Process
+In Python, there are inbuilt string methods such as is_digit() and is_alpha() that helps to check if the element is a digit or an alphabet.
 
+We can solve the problem by following these steps:
+
+Create counters for alphabets, digits, and symbols.
+Loop through each character in a string.
+By using the is_alpha() method, check if the character is the alphabet.
+If the character is the alphabet, increment the alphabet counter by 1.
+Again, by using the is_digit() method, check if the character is a digit.
+If the character is a digit, increment the digit counter by 1.
+If none of the above conditions satisfy, increment the symbol counter by 1.
+Finally, print the results of all counters.
+"""
+
+"""
+Challenge:
+Alphabets, Digits, and Symbols
+Easy
+Problem Description
+Create a Python program to count the alphabets, digits, and symbols in a string.
+
+Example
+Test Input
+
+for the string 'Hello123.'
+Expected Output
+
+Alphabets: 5
+Digits: 3
+Symbols: 1
+"""
 
